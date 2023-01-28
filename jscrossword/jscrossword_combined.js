f699d27ca976ca38dab8b4a469333226176ff2e0
Alex Boisvert <boisvert42@gmail.com>
2023-01-28 13:02:47 -0800

    fix solution thing

--- jscrossword/jscrossword_combined.js
+++ jscrossword/jscrossword_combined.js
@@ -203,6 +203,7 @@ function xw_read_ipuz(data) {
                 } else if (solution.cell) {
                     solution = solution.cell;
                 }
+                if (solution) {solution = solution.toUpperCase();}
 
             } catch {}
             // type
@@ -253,7 +254,7 @@ function xw_read_ipuz(data) {
             var new_cell = {
                 x: x,
                 y: y,
-                solution: solution.toUpperCase(),
+                solution: solution,
                 number: number,
                 type: type,
                 "background-color": background_color,
